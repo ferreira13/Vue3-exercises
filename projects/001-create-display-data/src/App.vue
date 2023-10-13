fecf3f9542447bd5b830cf78c2552af4c7e1e881
Atila Ferreira <atila.ferreira13@icloud.com>
2023-10-13 14:21:29 +0200

    WIP: add eslint, faker and setup basic list

--- projects/001-create-display-data/src/App.vue
+++ projects/001-create-display-data/src/App.vue
@@ -1,16 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1 v-once v-text="title"></h1>
+    <h2 v-text="subtitle"></h2>
+    <div>
+      <div v-if="todos.length">
+        <h3>There {{ todos.length > 1 ? `are ${todos.length} todos` : `is a todo` }}</h3>
+        <ul>
+          <li v-for="(todo, index) in todos" :key="index">{{ todo }}</li>
+        </ul>
+      </div>
+      <h3 v-else>There aren't any todos to display</h3>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { faker } from '@faker-js/faker'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'lv-tarefas',
+  data() {
+    return { title: 'todo list', subtitle: 'Set the description', todos: [] }
+  },
+  mounted() {
+    setInterval(() => this.todos.push(faker.lorem.sentences({ min: 1, max: 1 })), 2000)
+  },
 }
 </script>
 
@@ -19,8 +33,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* text-align: center; */
+  margin-top: 10px;
 }
 </style>
